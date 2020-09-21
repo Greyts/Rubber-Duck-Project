7f09529470c978febdc0fddd54873e8277630b8c
Gracjan <gracjan.miernicki@gmail.com>
2020-09-21 14:29:52 +0200

    Works with multiple urls, need to repair amount detection

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from get_url import *
 
 def ducks_in(is_duck: str): #checks how many ducks are sold together
 
+    ducks = 1
     string =''
     try:
         if 'szt' or 'sztuk' in is_duck:
@@ -17,15 +19,15 @@ def ducks_in(is_duck: str): #checks how many ducks are sold together
                         string += digit
 
                     ducks = int(string)
-        else:
-            ducks = 1
+        #else:
+            #ducks = 1
     except:
         ducks = 1
 
     return ducks
 
 
-def ducks_total(string_sold: str, ducks_together: int): #strips string to return integer of sold ducks
+def ducks_total(string_sold: str, ducks_together: int): #strips string to return integer of sold ducks (multiplies sold sets by amount of ducks in a single set)
 
     try:
         digits =[digit for digit in string_sold.split() if digit.isdigit()]
@@ -57,9 +59,9 @@ def get_ducks(soup): # getting the ducks and amount sold
 
     try:
         ducks_sold = soup.find('div', class_="_9a071_RRpJq _9a071_2sZmp _1h7wt").get_text().strip('<div>')
-        print(type(ducks_sold))
+
     except:
-        pass
+        ducks_sold = 0
 
     ducks_together = (ducks_in(is_duck))
     total_ducks = ducks_total(ducks_sold, ducks_together)
@@ -83,7 +85,8 @@ def main(url):
 
 if __name__ == '__main__':
 
-    #url = 'https://allegro.pl/oferta/tullo-gumowe-kaczuszki-do-kapieli-4szt-zolte-012-9240757162?bi_s=ads&bi_m=listing%3Adesktop%3Aquery&bi_c=MDZlN2ExYWItOWE0Zi00N2QyLTg0YzQtMmQwNDAyNmY3YmZiAA&bi_t=ape&referrer=proxy&emission_unit_id=1e96e31a-c04a-4345-840b-dba0c3b83d2d'
-    #url = 'https://allegro.pl/oferta/kaczuszka-gumowa-zabawka-do-kapieli-20szt-za1163-9608879290?bi_s=ads&bi_m=listing%3Adesktop%3Aqueryandcategory&bi_c=NjVlZDlhMzAtNjNjOS00NzA4LThlZjctMDJmZWIxY2ZhY2Y1AA&bi_t=ape&referrer=proxy&emission_unit_id=0167525d-b7cc-44c1-892c-9e510a31ac62'
-    url = 'https://allegro.pl/oferta/gumowa-kaczka-kaczuszka-zabawka-do-kapieli-biala-9553447581'
-    main(url)
+
+    links = get_list_of_pages(get_multiple_pages('https://allegro.pl/kategoria/zabawki-do-kapieli-19416?string=kaczuszka%20gumowa&bmatch=baseline-product-eyesa2-engag-dict45-bab-1-3-0717'))
+    for url in links:
+        main(url)
+
